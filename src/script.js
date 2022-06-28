4478f763f3ce543f82e9d09205671a8b0c000572
Zakir Hussain <zakirhussain1795@gmail.com>
2022-06-28 15:24:40 -0400

    update

--- src/script.js
+++ src/script.js
@@ -28,20 +28,67 @@ function fight(player1, player2, player1Health, player2Health) {
     if (attacker === player1) {
       player2Health = attackPlayer(player2Health);
       logHealth(player2, player2Health);
+      if (isDead(player2Health) === true) {
+        logDeath(player1, player2);
+        break;
+      }
     } else {
       player1Health = attackPlayer(player1Health);
       logHealth(player1, player1Health);
-    }
-
-    if (isDead(player2Health) === true) {
-      logDeath(player1, player2);
-      break;
-    }
-    if (isDead(player1Health) === true) {
-      logDeath(player2, player1);
-      break;
+      if (isDead(player1Health) === true) {
+        logDeath(player2, player1);
+        break;
+      }
     }
   }
 }
 
 fight("x", "y", 50, 60);
+
+// challenges
+
+const getGrade = (score) => {
+  if (score >= 90) {
+    return `A ${score}%`;
+  } else if (score < 90 && score >= 80) {
+    return `B ${score}%`;
+  } else if (score < 80 && score >= 70) {
+    return `C ${score}%`;
+  } else if (score < 70 && score >= 60) {
+    return `D ${score}%`;
+  } else {
+    return `F ${score}%`;
+  }
+};
+console.log(getGrade(90));
+
+const prioritize = (urgent, important) => {
+  if (urgent === true) {
+    if (important === true) {
+      return "Urgent and Important priority 1";
+    } else {
+      return "Urgent not important priority 3";
+    }
+  } else {
+    if (important === true) {
+      return "Important but not Urgent prioty 2";
+    } else {
+      return "Not important nor urgent";
+    }
+  }
+};
+
+console.log(prioritize(false, true));
+
+let totalpay = null;
+const calcuatePay = (wage, hours) => {
+  if (hours > 40) {
+    totalpay = wage * 40 + wage * 1.5 * (hours - 40);
+    return totalpay;
+  } else {
+    totalpay = wage * hours;
+    return totalpay;
+  }
+};
+
+console.log(calcuatePay(15, 45));
